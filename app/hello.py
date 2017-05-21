78d5226a0d931f54b4475eef194551c238eeb9f4
Sunil Kumar Mohanty <sunil.mohanty@aalto.fi>
2017-05-21 15:24:59 +0300

    face detection code

--- app/hello.py
+++ app/hello.py
@@ -2,8 +2,10 @@ import cv2
 import sys
 import urllib
 import time
+import json
 from firebase.firebase import FirebaseApplication, FirebaseAuthentication
 # Get user supplied values
+
 def main():
     imagePath = "/usr/src/app/photo.jpg"
     cascPath = "/usr/src/app/haarcascade_frontalface_default.xml"
@@ -11,16 +13,18 @@ def main():
     # Create the haar cascade
     faceCascade = cv2.CascadeClassifier(cascPath)
     i = 0
+    firebase = FirebaseApplication('https://facedetection-f5595.firebaseio.com', None)
     # Read the image
-    while(i < 0):
+    while(i < 5):
         i = i+1
         getFrame(camIP)
         img = cv2.imread(imagePath,0)
         faces = faceCascade.detectMultiScale(img, 1.3, 5)
         print("Found {0} faces! ".format(len(faces)))
+        firebase.post('/camera1','{faces:1}')
         time.sleep(1)
     authentication = FirebaseAuthentication('GtxD03WernNP8k0RF4SAbqaXzJNxzLQx77vCmEO6', 'sunilkumarmohanty@gmail.com',None)
-    firebase = FirebaseApplication('https://facedetection-f5595.firebaseio.com', authentication)
+
     user = authentication.get_user()
     print user.firebase_auth_token
     result = firebase.get('/cameras', None, params= {'print': 'pretty'})
@@ -30,5 +34,5 @@ def getFrame(Camera_IP):
         imageFile = urllib.URLopener()
         #print("http://"+ Camera_IP + ":8080/photo.jpg")
         imageFile.retrieve("http://"+ Camera_IP + ":8080/photo.jpg", '/usr/src/app/photo.jpg')
-
-main()
+if __name__ == '__main__':
+    main()
