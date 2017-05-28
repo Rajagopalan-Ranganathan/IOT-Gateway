674311110b91e9afc345dd2454e8ac3d9b46d9bc
Rajagopalan Ranganathan <rajagopalan.ranganathan@aalto.fi>
2017-05-28 14:23:25 +0300

    Added hashes :) comments

--- app/hello.py
+++ app/hello.py
@@ -14,6 +14,7 @@ import threading
 cameraList = []
 firebase = FirebaseApplication('https://facedetection-f5595.firebaseio.com', None)
 
+##################################
 def datafetcher():
     #i=10
     while(1):
